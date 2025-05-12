539ddfeee5a027ab425574ce761d8c0e20870df3
sergioAgui1035 <53014367+sergioAgui1035@users.noreply.github.com>
2025-05-12 07:20:41 -0500

    Commit Sergio Aguirre
    Se agregan métodos generales

---
+++ Metodos.java
@@ -0,0 +1,39 @@
+
+
+public class Metodos {
+
+    public void IngresarEstudiante(){
+
+        System.out.println("Se ingreso estiudiante exitosamente ");
+    }
+
+    public void RegistrarPrestamoEquipo(){
+
+        System.out.println("Se registro prestamo de equipo a estudiante : ");
+    }
+
+    public void ModificarPrestamoEquipo(){
+
+        System.out.println("Se modifico prestamo de equipo a estudiante : ");
+    }
+
+    public void DevolucionEquipo(){
+
+        System.out.println("Se registro devolucion de equipo a estudiante : ");
+    }
+
+    public void BuscarEquipo(){
+
+        System.out.println("El equipo : " + " Lo tiene el estudiante : ");
+    }
+
+    public void AgregarComputadorPortatil(){
+        
+        System.out.println("Se registro computador exitosamente.");
+    }
+
+    public void ImprimirInventarioTotal(){
+
+        System.out.println("Inventario total");
+    }
+}
