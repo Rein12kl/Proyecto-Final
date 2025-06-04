688039f0177cecbaa91a74a89dd1f04177cd8a9f
sergioAgui1035 <53014367+sergioAgui1035@users.noreply.github.com>
2025-06-03 22:00:47 -0500

    Update Prestamo.java

--- Prestamo.java
+++ Prestamo.java
@@ -1,6 +1,9 @@
+
 public class Prestamo {
     private Estudiantes estudiante;
     private Inventario inventario;
+    private String fechaPrestamo;
+    private String fechaDevolucion;
     
     public Estudiantes getEstudiante() {
         return estudiante;
@@ -13,6 +16,18 @@ public class Prestamo {
     }
     public void setInventario(Inventario inventario) {
         this.inventario = inventario;
+    }
+    public String getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+    public void setFechaPrestamo(String fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+    public String getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+    public void setFechaDevolucion(String fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
     }
 
 
